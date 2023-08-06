18e120776f253787ed1e9225442302605dda2605
Cristian <r.cristiano.ai@gmail.com>
2023-08-06 17:13:29 -0400

    Fixed minor issues in server.js file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api', async (req, res) => {
   }
 });
 
-const PORT = 5000
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
