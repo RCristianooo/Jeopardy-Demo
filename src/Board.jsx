e7112373fc58e320b255a60e6ea138df5170785d
Cristian <r.cristiano.ai@gmail.com>
2023-08-06 17:11:29 -0400

    Modified content on server.js, board, clue, and category files! Added css files that applies styling to board, category, and clue files!

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Category from "./Category"
+import React, { useState, useEffect } from 'react';
+import Category from './Category'
+import './board.css'
 
 function Board() {
-const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
     // This useEffect hook will run once after the component is first rendered.
 
@@ -11,34 +12,29 @@ const [categories, setCategories] = useState([])
         let offset = Math.floor(Math.random() * 28175) + 1
 
         // Fetch the categories from the API.
-        
-        const categoryURL = `https://localhost:6000/api/categories?count=6&offset=${offset}`
+        const categoryUrl = `http://localhost:5000/api/categories?count=6&offset=${offset}`;
 
-        const categoryResponse = await fetch(categoryURL)
-        const categoryData = await categoryResponse.json()
-        
-        console.error(error)
+        const categoryResponse = await fetch(categoryUrl);
+        const categoryData = await categoryResponse.json();
 
         // Fetch the clues for each category
         const categoriesWithClues = await Promise.all(categoryData.map(async (category) => {
-            const clueUrl = `https://localhost:6000/api/clues?category=${category.id}`
-              const clueResponse = await fetch(categoryURL)
-              const clueData = await clueResponse.json()
-
-              console.error(error)
+              const clueUrl = `http://localhost:5000/api/clues?category=${category.id}`;
+              const clueResponse = await fetch(clueUrl);
+              const clueData = await clueResponse.json();
             
             // Attach the clues to the category.
             return {
                 ...category,
                 clues: clueData,
-            }
-        }))
+            };
+        }));
         //Put categories and clues inside of state variable.
         setCategories(categoriesWithClues)
       }
 
-      fetchData()
-    }, [])
+      fetchData();
+    }, []);
     
     return (
         <div className="jeopardy-board">
@@ -46,5 +42,7 @@ const [categories, setCategories] = useState([])
                 <Category key={category.id} category={category} />
             ))}
         </div>
-    )
+    );
 }
+
+export default Board;
